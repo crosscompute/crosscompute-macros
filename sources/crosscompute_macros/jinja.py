86b1a076ca537ca2fc97b1f9c1d709111a107865
Roy Hyunjin Han <rhh@crosscompute.com>
2024-06-14 08:45:44 -0400

    Merge jinja from invisibleroads-macros-web

---
+++ sources/crosscompute_macros/jinja.py
@@ -0,0 +1,71 @@
+from os.path import dirname, getmtime, join, normpath, realpath
+from string import Template as StringTemplate
+
+from jinja2 import (
+    BaseLoader,
+    Environment,
+    Template as JinjaTemplate,
+    TemplateNotFound)
+
+
+class AssetStorage():
+
+    def __init__(self, folder):
+        self.folder = folder
+
+    def load_raw_text(self, file_name):
+        return (self.folder / file_name).read_text().strip()
+
+    def load_string_text(self, file_name):
+        return StringTemplate(self.load_raw_text(file_name))
+
+    def load_jinja_text(self, file_name):
+        return JinjaTemplate(self.load_raw_text(file_name), trim_blocks=True)
+
+
+class PathTemplateLoader(BaseLoader):
+
+    def __init__(self, encoding='utf-8'):
+        self.encoding = encoding
+
+    def get_source(self, environment, template):
+        'Support absolute template paths'
+        try:
+            modification_time = getmtime(template)
+        except (OSError, TypeError):
+            raise TemplateNotFound(template)
+
+        def is_latest():
+            try:
+                return modification_time == getmtime(template)
+            except OSError:
+                return False
+
+        with open(template, mode='rt', encoding=self.encoding) as f:
+            text = f.read()
+        return text, realpath(template), is_latest
+
+
+class RelativeTemplateEnvironment(Environment):
+
+    def join_path(self, template, parent):
+        'Support relative template paths via extends, import, include'
+        template = get_asset_path(template)
+        return normpath(join(dirname(
+            parent), template)) if template else template
+
+
+def get_asset_path(asset_uri):
+    asset_parts = asset_uri.split(':')
+    if len(asset_parts) > 1:
+        package_name, relative_path = asset_parts
+        package_name = package_name.strip()
+    else:
+        package_name, relative_path = '', asset_parts[0]
+    if package_name:
+        package = __import__(package_name)
+        package_folder = Path(package.__file__).parent
+        path = str(package_folder / relative_path)
+    else:
+        path = relative_path
+    return path
--- sources/crosscompute_macros/starlette.py
+++ sources/crosscompute_macros/starlette.py
@@ -1,7 +1,16 @@
 import asyncio
 from logging import getLogger
 
+from starlette.templating import Jinja2Templates
 from starlette.websockets import WebSocketDisconnect
+
+
+class TemplateResponseFactory(Jinja2Templates):
+
+    def __init__(self, environment, context_processors=None):
+        'Assume nothing about the template environment'
+        self.env = environment
+        self.context_processors = context_processors or []
 
 
 async def yield_bytes_while_connected(websocket, timeout_in_seconds):
