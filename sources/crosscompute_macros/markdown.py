584a6fc6f8ce6da9c1d229a79bf6c24045f712f2
Roy Hyunjin Han <rhh@crosscompute.com>
2024-07-29 07:36:49 -0400

    Restore remove_single_paragraph

--- sources/crosscompute_macros/markdown.py
+++ sources/crosscompute_macros/markdown.py
@@ -3,6 +3,7 @@ import re
 from markdown2 import markdown
 
 
+SINGLE_PARAGRAPH_PATTERN = re.compile(r'^<p>((?:(?!<p>).)*)</p>$', re.DOTALL)
 EXTRAS = [
     'break-on-newline',
     'code-friendly',
@@ -25,6 +26,10 @@ TAGS = [
 
 def get_html_from_markdown(text, extras=EXTRAS):
     return markdown(text, extras=extras).strip()
+
+
+def remove_single_paragraph(html):
+    return SINGLE_PARAGRAPH_PATTERN.sub(r'\g<1>', html)
 
 
 def remove_parent_paragraphs(html, tags=TAGS):
