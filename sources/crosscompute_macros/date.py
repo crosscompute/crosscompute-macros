0534147d9b7b0c8262090fdbab685aeeaa029384
Roy Hyunjin Han <rhh@crosscompute.com>
2024-10-23 15:15:32 -0400

    Define get_utc_now

--- sources/crosscompute_macros/date.py
+++ sources/crosscompute_macros/date.py
@@ -1,7 +1,13 @@
 import calendar
-from datetime import datetime
+from datetime import datetime, UTC
 
 
-def get_day_count(year=datetime.now().year):
+def get_day_count(year=None):
     # https://stackoverflow.com/a/67395758/192092
+    if year is None:
+        year = datetime.now().year
     return 365 + calendar.isleap(year)
+
+
+def get_utc_now():
+    return datetime.now(UTC)
