eb9e2ec0b2764859a43c477f2c15e5b8dcde5ae5
Roy Hyunjin Han <rhh@crosscompute.com>
2025-02-27 17:07:48 -0500

    Initialize from instance

--- sources/crosscompute_macros/abstract.py
+++ sources/crosscompute_macros/abstract.py
@@ -1,4 +1,7 @@
 class Clay:
 
-    def __init__(self, **kwargs):
-        self.__dict__.update(kwargs)
+    def __init__(self, instance=None, **kwargs):
+        d = self.__dict__
+        if instance:
+            d.update(instance.__dict__)
+        d.update(kwargs)
