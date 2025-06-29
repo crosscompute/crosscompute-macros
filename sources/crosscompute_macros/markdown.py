df3717546695d2d23f2f925c6b1e74e938afd719
Roy Hyunjin Han <rhh@crosscompute.com>
2025-06-29 02:42:23 -0400

    Replace remove_parent_paragraphs with remove_inner_paragraphs

--- sources/crosscompute_macros/markdown.py
+++ sources/crosscompute_macros/markdown.py
@@ -3,7 +3,7 @@ import re
 from markdown2 import markdown
 
 
-SINGLE_PARAGRAPH_PATTERN = re.compile(r'^<p>((?:(?!<p>).)*)</p>$', re.DOTALL)
+OUTER_PARAGRAPH_PATTERN = re.compile(r'^<p>((?:(?!<p>).)*)</p>$', re.DOTALL)
 EXTRAS = [
     'break-on-newline',
     'code-friendly',
@@ -17,25 +17,17 @@ EXTRAS = [
     'tables',
     'use-file-vars',
     'task_list']
-TAGS = [
-    'a',
-    'button',
-    'div',
-    'input',
-    'span']
 
 
 def get_html_from_markdown(text, extras=EXTRAS):
     return markdown(text, extras=extras).strip()
 
 
-def remove_single_paragraph(html):
-    return SINGLE_PARAGRAPH_PATTERN.sub(r'\g<1>', html)
+def remove_outer_paragraph(html):
+    return OUTER_PARAGRAPH_PATTERN.sub(r'\g<1>', html)
 
 
-def remove_parent_paragraphs(html, tags=TAGS):
-    if tags:
-        tags_text = '|'.join(tags)
-        html = re.sub(r'<p>(\s*<(?:%s))' % tags_text, r'\g<1>', html)
-        html = re.sub(r'((?:%s)[^>]*>\s*)</p>' % tags_text, r'\g<1>', html)
+def remove_inner_paragraphs(html, a, b):
+    html = re.sub(r'<p>(\s*(?:[^<X]*X))'.replace('X', a), r'\g<1>', html)
+    html = re.sub(r'((?:X[^>]*)\s*)</p>'.replace('X', b), r'\g<1>', html)
     return html
