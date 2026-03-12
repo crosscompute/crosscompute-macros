556488a84d64d00781be4bd19aed2d099d96126f
Roy Hyunjin Han <rhh@crosscompute.com>
2026-03-12 17:08:05 -0400

    Save csv

---
+++ sources/crosscompute_macros/csv.py
@@ -0,0 +1,20 @@
+from csv import DictReader, DictWriter
+
+
+def sync_yield_dictionaries_from_csv(source_path):
+    with source_path.open('rt') as f:
+        for d in DictReader(f):
+            yield d
+
+
+def sync_load_dictionaries_from_csv(source_path):
+    return list(sync_yield_dictionaries_from_csv(source_path))
+
+
+def sync_save_dictionaries_to_csv(target_path, ds, keys=None):
+    if not keys:
+        keys = ds[0].keys() if ds else []
+    with target_path.open('wt') as f:
+        csv_writer = DictWriter(f, fieldnames=keys)
+        csv_writer.writeheader()
+        csv_writer.writerows(ds)
