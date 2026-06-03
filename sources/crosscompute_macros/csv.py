30d99acd745ed751b75014b1ea35a4d7f492a232
Roy Hyunjin Han <rhh@crosscompute.com>
2026-06-03 15:13:26 -0400

    Pass encoding

--- sources/crosscompute_macros/csv.py
+++ sources/crosscompute_macros/csv.py
@@ -6,14 +6,16 @@ def sync_yield_dictionaries_from_csv(source_path, encoding='utf-8'):
         yield from DictReader(f)
 
 
-def sync_load_dictionaries_from_csv(source_path):
-    return list(sync_yield_dictionaries_from_csv(source_path))
+def sync_load_dictionaries_from_csv(source_path, encoding='utf-8'):
+    return list(sync_yield_dictionaries_from_csv(
+        source_path, encoding=encoding))
 
 
-def sync_save_dictionaries_to_csv(target_path, ds, keys=None):
+def sync_save_dictionaries_to_csv(
+        target_path, ds, keys=None, encoding='utf-8'):
     if not keys:
         keys = ds[0].keys() if ds else []
-    with target_path.open('wt') as f:
+    with target_path.open('wt', encoding=encoding) as f:
         csv_writer = DictWriter(f, fieldnames=keys)
         csv_writer.writeheader()
         csv_writer.writerows(ds)
