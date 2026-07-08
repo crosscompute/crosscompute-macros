89cb57b9502658ff25d3ae9c9792cef7cc6e3e7c
Roy Hyunjin Han <rhh@crosscompute.com>
2026-07-08 12:17:20 -0400

    Replace bytes with text

--- sources/crosscompute_macros/process.py
+++ sources/crosscompute_macros/process.py
@@ -7,27 +7,34 @@ from .error import ProcessError
 
 
 class ProcessPack(NamedTuple):
-    output_bytes: bytes
-    error_bytes: bytes
+    output_text: str
+    error_text: str
 
 
-async def run_process(args, cwd=None, env=None, input_bytes=None):
+async def run_process(args, cwd=None, env=None, input_text=None):
     kwargs = {}
-    if input_bytes:
+    if input_text:
         kwargs['stdin'] = subprocess.PIPE
     kwargs['stdout'] = subprocess.PIPE
     kwargs['stderr'] = subprocess.PIPE
     p = await asyncio.create_subprocess_exec(*args, cwd=cwd, env=env, **kwargs)
-    output_bytes, error_bytes = await p.communicate(input_bytes)
-    if return_code := p.returncode:
+    output_bytes, error_bytes = await p.communicate(
+        input_text.encode() if input_text else None)
+    output_text = output_bytes.decode()
+    error_text = error_bytes.decode()
+    return_code = p.returncode
+    L.debug(
+        f"run_process {args=} {return_code=} "
+        f"output_text='''{output_text}''' "
+        f"error_text='''{error_text}'''")
+    if return_code:
         raise ProcessError(
             return_code=return_code,
-            output_bytes=output_bytes,
-            error_bytes=error_bytes)
-    L.debug(f'run_process {args=} {output_bytes=} {error_bytes=}')
+            output_text=output_text,
+            error_text=error_text)
     return ProcessPack(
-        output_bytes=output_bytes,
-        error_bytes=error_bytes)
+        output_text=output_text,
+        error_text=error_text)
 
 
 L = getLogger(__name__)
